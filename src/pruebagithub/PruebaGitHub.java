07e36b5fe0c001290f01fbdbeb03843c72f32471
Osthinavy <oscaralejandroandino@gmail.com>
2023-08-08 20:07:23 -0300

    Initial commit

---
+++ src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author Educacion 4.0
+ */
+public class PruebaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
